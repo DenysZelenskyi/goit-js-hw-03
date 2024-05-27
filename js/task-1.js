99ef3387f7529fb09d3efa76af3cf7650097c8fd
DenysZelenskyi <den.zelenskiy@gmail.com>
2024-05-27 14:14:02 -0700

    add hw 3

---
+++ js/task-1.js
@@ -0,0 +1,20 @@
+'use strict';
+
+// Напиши функцію slugify(title), яка приймає заголовок статті, параметр title і повертає slug, створений із цього рядка.
+
+// Значенням параметра title будуть рядки, слова яких розділені лише пробілами.
+// Усі символи slug повинні бути в нижньому регістрі.
+// Усі слова slug повинні бути розділені тире.
+// Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи. У консоль будуть виведені результати її роботи.
+
+
+const slugify = function (title) {
+  const strLower = title.toLowerCase();
+  const kebabCase = strLower.split(' ').join('-');
+  return kebabCase;
+};
+
+console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
+console.log(slugify("English for developer")); // "english-for-developer"
+console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
